ffa1c3319bafdf326f8729035fd6f755ff77e5a2
James McCarthy <mccarthyjames35@yahoo.com>
2024-01-17 10:43:21 -0800

    add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,35 @@
 /** @format */
 
+// pages/login/page.tsx
 import React from 'react';
 
-const page = () => {
-	return <div>page</div>;
+const LoginPage = () => {
+	return (
+		<div className="container" style={{width: '25%', margin: '0 auto'}}>
+			<h1>Login</h1>
+			<form style={{width: '100%'}}>
+				<div className="form-group">
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						id="email"
+						placeholder="Enter your email"
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						id="password"
+						placeholder="Enter your password"
+					/>
+				</div>
+				<button type="submit" className="button">
+					Log In
+				</button>
+			</form>
+		</div>
+	);
 };
 
-export default page;
+export default LoginPage;
